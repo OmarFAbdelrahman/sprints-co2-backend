fbcd106430b90fe07ac3e38d000495686d602193
Omar Farouk <omar__farouk@hotmail.com>
2022-06-08 23:02:01 +0200

    added Mongo  DB connection

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sprints-co2-backend
+
+# DB Access
+
+You need to add Atlas Mongo connection url in a .env file
--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, console.log(`server is listening on port: ${port}...`));
   } catch (error) {
     console.log(error);
@@ -12,4 +16,3 @@ const start = async () => {
 };
 
 start();
-console.log("E-Commerce API");
